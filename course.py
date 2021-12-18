803cdc3be0aad8f47603a3e4e297f8986f36dd90
a.barkook <alibarkook@gmail.com>
2021-12-18 21:32:20 +0330

    feat -> implement course class

---
+++ course.py
@@ -0,0 +1,12 @@
+class Course():
+
+    def __init__(self, courseName, teacherName, instituteName, courseLink, coursePrice, courseSessionCount, courseTime):
+
+        self.courseName = courseName
+        self.teacherName = teacherName
+        self.instituteName = instituteName
+        self.courseLink = courseLink
+        self.coursePrice = coursePrice
+        self.courseSessionCount = courseSessionCount
+        self.courseTime = courseTime
+    
