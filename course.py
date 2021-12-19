03af712dcbc9c4e4ade4404e90af2d27b92fbc28
a.barkook <alibarkook@gmail.com>
2021-12-19 19:21:54 +0330

    update -> get all data about course create an instanse class

--- course.py
+++ course.py
@@ -1,12 +1,11 @@
 class Course():
 
-    def __init__(self, courseName, teacherName, instituteName, courseLink, coursePrice, courseSessionCount, courseTime):
+    def __init__(self, courseName, teacherName, instituteName, courseLink, coursePrice, courseSessionCountOrTime):
 
         self.courseName = courseName
         self.teacherName = teacherName
         self.instituteName = instituteName
         self.courseLink = courseLink
         self.coursePrice = coursePrice
-        self.courseSessionCount = courseSessionCount
-        self.courseTime = courseTime
+        self.courseSessionCountOrTime = courseSessionCountOrTime
     
--- maktabkhoone.py
+++ maktabkhoone.py
@@ -9,17 +9,34 @@ siteUrl = 'https://maktabkhooneh.org'
 # ? get courses info
 def getCouresesInformation(couresLinkList):
     totalCourse = []
-    courseResponse = requests.get('https://maktabkhooneh.org/course/%D8%A2%D9%85%D9%88%D8%B2%D8%B4-%D8%B1%D8%A7%DB%8C%DA%AF%D8%A7%D9%86-%D8%A2%D9%85%D8%A7%D8%B1-%D8%A7%D8%AD%D8%AA%D9%85%D8%A7%D9%84-mk1365/')
+    courseResponse = requests.get('https://maktabkhooneh.org/course/%D8%A2%D9%85%D9%88%D8%B2%D8%B4-%D8%B1%D8%A7%DB%8C%DA%AF%D8%A7%D9%86-%D8%A2%D9%85%D8%A7%D8%B1-%D8%A7%D8%AD%D8%AA%D9%85%D8%A7%D9%84-mk1365/#seasons')
     courseHtml = BeautifulSoup(courseResponse.text, 'html.parser')
-    # courseHtml.findChild(right-padding)
-    x = courseHtml.select_one('.course-page')
-    
-    print(x)
-    # for coureslink in couresLinkList:
-        # print(siteUrl + coureslink)
 
-        # course = Course()
-        # totalCourse.append(course)
+    
+    title = courseHtml.title.get_text()
+    teacher = courseHtml.find_all(class_='teacher-card__image')[0]["title"]
+    intitute = courseHtml.find_all(class_='teacher-card__image')[1]["title"]
+    session = courseHtml.find(class_="chapter__clock-text").get_text()
+    price = ''
+
+    # ? check price
+    if courseHtml.find(class_="fl2"):
+        price = courseHtml.find(class_="fl2").get_text()
+    else:
+        price = 'رایگان'
+
+
+    course = Course(title, teacher, intitute, 'https://maktabkhooneh.org/course/%D8%A2%D9%85%D9%88%D8%B2%D8%B4-%D8%B1%D8%A7%DB%8C%DA%AF%D8%A7%D9%86-%D8%A2%D9%85%D8%A7%D8%B1-%D8%A7%D8%AD%D8%AA%D9%85%D8%A7%D9%84-mk1365/#seasons', price, session)
+    totalCourse.append(course)
+    print(totalCourse)
+    
+
+    # Html_file= open("htmlDom2.txt","w", encoding="utf8")
+    # Html_file.write(str(courseHtml.prettify()))
+    # Html_file.close()
+
+
+
 
 
 
